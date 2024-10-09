d57c6f22d0f783b07c27fc70c9d85e3d5ea95b90
ArmanDev1010 <arman.manukyan24.y@gmail.com>
2024-10-09 15:40:48 +0400

    blank - template 5 added

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,8 @@ const App = () => {
             {/* <Route path="" element={<Home5 codename="f9" />} /> */}
             {/* <Route path="" element={<Home5 codename="wta" />} /> */}
             {/* <Route path="" element={<Home5 codename="raw" />} /> */}
-            <Route path="" element={<Home5 codename="larcc" />} />
+            {/* <Route path="" element={<Home5 codename="larcc" />} /> */}
+            <Route path="" element={<Home5 codename="blank" />} />
           </Route>
         </Routes>
       </MyContext.Provider>
