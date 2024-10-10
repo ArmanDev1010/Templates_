db789a2530b0650894ce5ff190469125f55f5e13
ArmanDev1010 <arman.manukyan24.y@gmail.com>
2024-10-10 12:02:06 +0400

    houser - template 5 added

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,8 @@ const App = () => {
             {/* <Route path="" element={<Home5 codename="schedio" />} /> */}
             {/* <Route path="" element={<Home5 codename="watsuk" />} /> */}
             {/* <Route path="" element={<Home5 codename="unk" />} /> */}
-            <Route path="" element={<Home5 codename="plearn" />} />
+            {/* <Route path="" element={<Home5 codename="plearn" />} /> */}
+            <Route path="" element={<Home5 codename="houser" />} />
           </Route>
         </Routes>
       </MyContext.Provider>
